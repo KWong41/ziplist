adfba51ad9059e64d11e89fc31160e412654df39
KWong41 <46613890+KWong41@users.noreply.github.com>
2019-09-23 14:43:46 -1000

    Update ziplist.js

--- ziplist.js
+++ ziplist.js
@@ -1,7 +1,7 @@
 //  http://courses.ics.hawaii.edu/ics314f19/morea/coding-standards/experience-five-problems-2.html
 //  pWOD 2:Coding Standards
 //  First Attempt:25 mins (DNF)
-//  Second Attempt (watching Dr. Johnson's video for support):
+//  Second Attempt (watching Dr. Johnson's video for support):  22:10:27 (completed)
 //
 //
 //
@@ -27,6 +27,30 @@ function zipListTheSimpleWay(){
 */
 
 //  Second attempt
+/*
+
+/*
+
+const testList1 = ['a', 'b', 'c'];
+const testList2 = [1, 2, 3];
+
+function zipList(list1, list2) {
+  const resultList = [];
+  for (let i = 0; i < list1.length; i++) {
+    resultList.push(list1[i], list2[i]);
+  }
+  return resultList;
+}
+
+console.log(zipList(testList1, testList2));
+
+function zipListTheSimpleWay(list1, list2) {
+  return _.flatten(_.zip(list1, list2));
+}
+
+console.log(zipListTheSimpleWay(testList1, testList2));
+
+*/
 
 const testList1 = ['a', 'b', 'c'];
 const testList2 = [1, 2, 3];
